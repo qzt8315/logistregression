654f269d96882751e6095a374ebdcdfcc47c3a13
qin <qinzhtao@163.com>
2018-01-13 21:06:32 +0800

    添加logistic函数

--- main.py
+++ main.py
@@ -2,6 +2,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+def sigmoid(z):
+    return 1./(1 + np.exp(-z))
+
 # 程序开始
 if __name__ == "__main__":
     # 从文本文件中读取数据
@@ -17,12 +20,14 @@ if __name__ == "__main__":
     # 测试调整的结果
     # print(data)
     # 显示整个数据集
-    X = data[:, 1:3]
-    y = data[:, 3]
-    plt.title('watermelon data')
-    plt.xlabel('density')
-    plt.ylabel('ratio_sugar')
-    plt.scatter(X[y == 0, 0], X[y == 0, 1], marker='o', color='k', s=100, label='bad')
-    plt.scatter(X[y == 1, 0], X[y == 1, 1], marker='o', color='g', s=100, label='good')
-    plt.legend(loc='upper right')
-    plt.show()
+    # X = data[:, 1:3]
+    # y = data[:, 3]
+    # plt.title('watermelon data')
+    # plt.xlabel('density')
+    # plt.ylabel('ratio_sugar')
+    # plt.scatter(X[y == 0, 0], X[y == 0, 1], marker='o', color='k', s=100, label='bad')
+    # plt.scatter(X[y == 1, 0], X[y == 1, 1], marker='o', color='g', s=100, label='good')
+    # plt.legend(loc='upper right')
+    # plt.show()
+    # 测试sigmoid函数
+    print(sigmoid(-100))
